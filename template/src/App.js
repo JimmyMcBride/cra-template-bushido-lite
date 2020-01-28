450b1783aa1467a05f716c279f57f67d040d0511
JimmyMcBride <mcbride967@gmail.com>
2020-01-27 23:25:05 -0600

    updated readme and code to new style standards

--- template/src/App.js
+++ template/src/App.js
@@ -9,12 +9,24 @@ import { AppWrapper } from 'bushido-strap';
 // Importing all routes
 import Dashboard from './components/Dashboard';
 
-const App = () => {
+// Using Web Font Loader for google fonts
+import WebFont from 'webfontloader';
+
+// setting our font variables
+const h_font = 'Comfortaa';
+const r_font = 'Montserrat';
+
+// using WebFont to easily access Google fonts
+WebFont.load({
+  google: {
+    families: [h_font, r_font],
+  },
+});
+
+export default function App() {
   return (
-    <AppWrapper>
+    <AppWrapper head_font={h_font} font={r_font}>
       <Route path="/" exact component={Dashboard} />
     </AppWrapper>
   );
-};
-
-export default App;
+}
