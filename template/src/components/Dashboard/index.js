3a9e2d2c2687037a054cbce2d23c13b1580f766a
JimmyMcBride <mcbride967@gmail.com>
2020-01-31 15:27:54 -0600

    touch up

--- template/src/components/Dashboard/index.js
+++ template/src/components/Dashboard/index.js
@@ -5,7 +5,7 @@ import { Wrapper, Button, Card } from 'bushido-strap';
 export default function Dashboard() {
   return (
     <Wrapper>
-      <Card shade>
+      <Card>
         <h1>Hello, world!</h1>
         <Button onClick={() => alert('Thanks for clicking me!')}>
           Click me!!!
